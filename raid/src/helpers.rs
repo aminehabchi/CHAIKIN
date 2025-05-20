12cb7358ad150f977b5d9fb6c39e3012932c0c7f
Your Name <you@example.com>
2025-05-20 16:24:14 +0100

    hello fixing

--- raid/src/helpers.rs
+++ raid/src/helpers.rs
@@ -1,7 +1,7 @@
 use super::geometrical_shapes::*;
 
-pub const WIDTH:usize=1000;
-pub const HEIGHT:usize=1000;
+pub const WIDTH: usize = 1000;
+pub const HEIGHT: usize = 1000;
 
 pub fn get_25(p1: Point, p2: Point) -> Point {
     let t = 0.25;
@@ -17,28 +17,28 @@ pub fn clear_window(buffer: &mut [u32]) {
     }
 }
 
-pub fn chakin(points:Vec<Point>)->Vec<Point>{
+pub fn chakin(points: Vec<Point>) -> Vec<Point> {
     let mut new_points: Vec<Point> = Vec::new();
-            new_points.push(points[0].clone());
-            for i in 1..points.len() {
-                let q = get_25(points[i - 1].clone(), points[i].clone());
-                let r = get_25(points[i].clone(), points[i - 1].clone());
+    new_points.push(points[0].clone());
+    for i in 1..points.len() {
+        let q = get_25(points[i - 1].clone(), points[i].clone());
+        let r = get_25(points[i].clone(), points[i - 1].clone());
 
-                new_points.push(q);
-                new_points.push(r);
-            }
-            new_points.push(points[points.len() - 1].clone());
+        new_points.push(q);
+        new_points.push(r);
+    }
+    new_points.push(points[points.len() - 1].clone());
 
-            new_points.clone()
+    new_points.clone()
 }
 
-pub fn draw_lines(points:&Vec<Point>,buffer:&mut [u32]){
+pub fn draw_lines(points: &Vec<Point>, buffer: &mut [u32]) {
     for i in 1..points.len() {
-        Line::new(points[i - 1].clone(), points[i].clone()).draw(
-            buffer,
-            WIDTH,
-            HEIGHT,
-            0xFFFFFF,
-        )
+        Line::new(points[i - 1].clone(), points[i].clone()).draw(buffer, WIDTH, HEIGHT, 0xFFFFFF)
+    }
+}
+pub fn draw_points(points: &Vec<Point>, buffer: &mut [u32]) {
+    for i in 0..points.len()  {
+        Circle::new(points[i].clone(),3).draw(buffer, WIDTH, HEIGHT, 0xFFFFFF);
     }
 }
--- raid/src/main.rs
+++ raid/src/main.rs
@@ -8,9 +8,7 @@ use geometrical_shapes::*;
 mod helpers;
 use helpers::*;
 
-
 fn main() {
-
     let mut buffer = vec![0_u32; WIDTH * HEIGHT];
     let mut window = Window::new("Moving Pixel", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
 
@@ -18,42 +16,56 @@ fn main() {
 
     let mut last_x: f32 = -1.0;
     let mut last_y: f32 = -1.0;
-#[allow(unused_variables)]
-    let mut step: u8 = 0;
+    #[allow(unused_variables)]
+    let mut step: u8 = 1;
     let mut points: Vec<Point> = Vec::new();
     let mut animate = false;
-
+    let mut count = 0;
+    let mut go = true;
     while window.is_open() && !window.is_key_down(Key::Escape) {
-
         // clear window if click on space
+        thread::sleep(Duration::from_millis(16));
+
         if window.is_key_down(Key::Space) {
             clear_window(&mut buffer);
             arr = Vec::new();
             points = Vec::new();
             step = 0;
+            go = true;
+            count = 0;
             animate = false;
+        }
+        if count == 20 {
+            count = 0;
+            go = true;
+        }
+        if !go {
+            count += 1;
+            continue;
         }
 
         //
-        if window.is_key_down(Key::Enter) &&!animate && arr.len()>0 {
-            
-            for i in 0..arr.len() - 1 {
-                Line::new(arr[i].clone(), arr[i + 1].clone()).draw(
-                    &mut buffer,
-                    WIDTH,
-                    HEIGHT,
-                    0xFFFFFF,
-                );
-            }
+        if window.is_key_down(Key::Enter) && !animate && arr.len() > 0 {
             points = arr.clone();
-            animate = true
+            animate = true;
+            draw_lines(&points, &mut buffer);
+            window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+            go =false;
+            continue;
         }
-        
-        if animate && step<8 {
+
+        if animate {
+            go = false;
             step += 1;
-            points=chakin(points);
+            if step == 8 {
+                step = 0;
+                points = arr.clone();
+            } else {
+                points = chakin(points);
+            }
             clear_window(&mut buffer);
-            draw_lines(&points,&mut buffer);
+            draw_lines(&points, &mut buffer);
+            draw_points(&arr, &mut buffer);
         }
 
         if window.get_mouse_down(MouseButton::Left) {
@@ -69,8 +81,6 @@ fn main() {
                 }
             }
         }
-
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
-        thread::sleep(Duration::from_millis(32));
     }
 }
